a45af57f95685dd0a68b5f99f7ac3891ae9fc9c5
Joshua Briley <josh@thebrileys.com>
2016-01-06 22:06:55 -0500

    Attempt array push method of combining operators and numbers.

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,14 @@
 (function(){
   var display = [];
+  var calc = [];
   var displayContainer = $('.display');
   var $button = $('.button')
-
+  function clearDisplay(){
+    display = [];
+  }
+  function clearNumbers(){
+    calc = [];
+  }
   function displayNumbers(){
     if(display.length === 0) {
       displayContainer.text("0");
@@ -11,23 +17,40 @@
     }
   }
 
-  function clearDisplay(){
-    display = [];
+  function arrayManipulation(){
+    // Split array at
+      // multiply
+      // divide
+      // add
+      // subract
+    // Join Numbers (including decimals)
   }
-  
-
 
   // User Actions
+  $('[data-operator]').on("click", function(){
+    var operationValue = $(this).attr('data-operator');
+    if(operationValue !== "equal"){
+      clearDisplay();
+      displayNumbers();
+      calc.push(operationValue);
+    } else if(operationValue === "equal") {
+      if(calc.length){
+        displayContainer.text("Answer!");
+      }
+    }
+  });
 
   $('[data-number]').on("click", function(){
     var buttonValue = $(this).attr("data-number");
     display += buttonValue;
+    calc.push(buttonValue);
     displayNumbers();
   });
 
-  $('[data-operator="clear"]').on("click", function(){
+  $('[data-memory="clear"]').on("click", function(){
     clearDisplay();
+    clearNumbers();
     displayNumbers();
-  })
+  });
 
 })();
